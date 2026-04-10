925a22a093b8974d7d97638d75354e0b44438f3d
AbhiramNetha <abhiramnetha17@gmail.com>
2026-04-10 17:50:23 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          fiber: ['@react-three/fiber', '@react-three/drei'],
+        },
+      },
+    },
+  },
 })
